168a8709f108eecd36df8eb317fc757aa6939da2
김치훈 <kimchihun@Mac.Davolink>
2025-07-09 14:56:17 +0900

    자유게시판 목록 더미데이터 적용

--- src/components/units/board/list/BoardList.container.js
+++ src/components/units/board/list/BoardList.container.js
@@ -1,54 +1,72 @@
 // 게시판 목록 container
 
-import BoardListUI from "./BoardList.presenter";
-import { useQuery } from "@apollo/client";
-import {
-  FETCH_BOARDS,
-  FETCH_BOARDS_COUNT,
-  FETCH_BOARDS_OF_THE_BEST,
-} from "./BoardList.queries";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import BoardListUI from './BoardList.presenter';
+import { useQuery } from '@apollo/client';
+import { FETCH_BOARDS, D_FETCH_BOARDS, FETCH_BOARDS_COUNT, FETCH_BOARDS_OF_THE_BEST, D_FETCH_BOARDS_OF_THE_BEST } from './BoardList.queries';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 const BoardList = () => {
   const router = useRouter();
-  const [keyword, setKeyword] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+  const [keyword, setKeyword] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
 
-  const { data: BestBoards } = useQuery(FETCH_BOARDS_OF_THE_BEST);
+  const [DFetchBoardsOfTheBestdata, setDFetchBoardsOfTheBestdata] = useState('');
+  const [DfetchBoardsData, setFetchBoardsData] = useState('');
 
-  const { data: fetchBoardsData, refetch } = useQuery(FETCH_BOARDS, {
+  // 베스트 게시물
+  // const { data: BestBoards } = useQuery(FETCH_BOARDS_OF_THE_BEST);
+  // 더미 베스트 게시물
+  useEffect(() => {
+    D_FETCH_BOARDS_OF_THE_BEST().then((result) => {
+      setDFetchBoardsOfTheBestdata(result.data);
+    });
+  }, []);
+
+  // 게시물 리스트
+  // const { data: fetchBoardsData, refetch } = useQuery(FETCH_BOARDS, {
+  //   variables: {
+  //     search: keyword,
+  //   },
+  // });
+
+  // 더미 자유게시판 게시글 목록 리스트
+  // refetch 흉내 낼 함수
+  const refetch = async () => {
+    const result = await D_FETCH_BOARDS();
+    setFetchBoardsData(result.data);
+  };
+  useEffect(() => {
+    refetch();
+  }, [keyword]);
+
+  // 게시물 리스트 갯수
+  const { data: dataBoardsCount, refetch: refetchBoardsCount } = useQuery(FETCH_BOARDS_COUNT, {
     variables: {
+      // startDate,
+      // endDate,
       search: keyword,
     },
   });
 
-  const { data: dataBoardsCount, refetch: refetchBoardsCount } = useQuery(
-    FETCH_BOARDS_COUNT,
-    {
-      variables: {
-        // startDate,
-        // endDate,
-        search: keyword,
-      },
-    }
-  );
-
+  // 각각 게시물 들어가기
   const onClickMoveToBoardDetail = (event) => {
-    if (event.target instanceof Element)
-      router.push(`/boards/${event.currentTarget.id}`);
+    if (event.target instanceof Element) router.push(`/boards/${event.currentTarget.id}`);
   };
 
+  // 게시물 생성버튼기능
   const onClickMoveToBoardNew = () => {
-    router.push("/boards/new");
+    router.push('/boards/new');
   };
 
   return (
     <BoardListUI
-      fetchBoardsData={fetchBoardsData}
+      // fetchBoardsData={fetchBoardsData}
+      fetchBoardsData={DfetchBoardsData} // 더미데이터
       refetch={refetch}
-      BestBoards={BestBoards?.fetchBoardsOfTheBest}
+      // BestBoards={BestBoards?.fetchBoardsOfTheBest}
+      BestBoards={DFetchBoardsOfTheBestdata?.fetchBoardsOfTheBest} //더미데이터
       onClickMoveToBoardDetail={onClickMoveToBoardDetail}
       refetchBoardsCount={refetchBoardsCount}
       count={dataBoardsCount?.fetchBoardsCount}
--- src/components/units/board/list/BoardList.presenter.js
+++ src/components/units/board/list/BoardList.presenter.js
@@ -1,12 +1,12 @@
 // 게시판 목록 presenter
 
-import * as S from "./BoardList.styles";
-import { getDatecomma } from "../../../../commons/libraries/utils";
-import { v4 as uuidv4 } from "uuid";
-import Searchbars02 from "../../../commons/searchbars/02/Searchbars02.container";
-import BoardListBestBoards from "./BoardList.presenterBestBoards";
-import Paginations01 from "../../../commons/paginations/paginations/01/Paginations01.container";
-import { PencilFillsvg } from "../../../../commons/styles/Iconsvg";
+import * as S from './BoardList.styles';
+import { getDatecomma } from '../../../../commons/libraries/utils';
+import { v4 as uuidv4 } from 'uuid';
+import Searchbars02 from '../../../commons/searchbars/02/Searchbars02.container';
+import BoardListBestBoards from './BoardList.presenterBestBoards';
+import Paginations01 from '../../../commons/paginations/paginations/01/Paginations01.container';
+import { PencilFillsvg } from '../../../../commons/styles/Iconsvg';
 
 const BoardListUI = (pr) => {
   return (
@@ -14,11 +14,7 @@ const BoardListUI = (pr) => {
       <S.BestText>베스트 게시글</S.BestText>
       <S.BestBoards>
         {pr.BestBoards?.map((el) => (
-          <BoardListBestBoards
-            key={uuidv4()}
-            el={el}
-            onClickMoveToBoardDetail={pr.onClickMoveToBoardDetail}
-          />
+          <BoardListBestBoards key={uuidv4()} el={el} onClickMoveToBoardDetail={pr.onClickMoveToBoardDetail} />
         ))}
       </S.BestBoards>
 
@@ -44,13 +40,13 @@ const BoardListUI = (pr) => {
             <S.HText>날짜</S.HText>
           </S.Grid>
 
-          {pr.fetchBoardsData?.fetchBoards.map((el, index) => (
+          {pr.fetchBoardsData?.fetchBoards?.map((el, index) => (
             <S.Grid key={uuidv4()}>
               <S.Text>{index + 1}</S.Text>
               <S.Title onClick={pr.onClickMoveToBoardDetail} id={el._id}>
                 {el.title
                   .replaceAll(pr.keyword, `#$%${pr.keyword}#$%`)
-                  .split("#$%")
+                  .split('#$%')
                   .map((el) => (
                     <S.Word key={uuidv4()} isMatched={pr.keyword === el}>
                       {el}
@@ -66,12 +62,7 @@ const BoardListUI = (pr) => {
         <S.Footer>
           <Paginations01 count={pr.count} refetch={pr.refetch} />
           <S.Button onClick={pr.onClickMoveToBoardNew}>
-            <PencilFillsvg
-              margin="0 11px 0 0"
-              width="18"
-              height="18"
-              fill="#000"
-            />
+            <PencilFillsvg margin='0 11px 0 0' width='18' height='18' fill='#000' />
             게시물 등록하기
           </S.Button>
         </S.Footer>
